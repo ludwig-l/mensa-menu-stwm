e144c27143dbd46d7ad86ed9188685815c1aa870
Ludwig Lohmer <ludwig-lohmer@web.de>
2022-07-10 16:28:52 +0200

    Add usage of current date using datetime library
    uses current day and checks if the current day is a weekday at all

--- testing.py
+++ testing.py
@@ -1,12 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import date
+import sys
+  
+# Returns the current local date
+date_today = date.today()
 
+# check if today is a weekday
+if date_today.weekday() >= 5:
+    print('Today is not a weekday. The mensa is not open today.')
+    sys.exit()
 
 # definitions
-date = '2022-07-08'
 mensa_dict = {'Mensa Arcisstraße': 421, 'Mensa Leopoldstraße': 411, 'Mensa Garching': 422}
 mensa_name = 'Mensa Arcisstraße'
-url = f'https://www.studentenwerk-muenchen.de/mensa/speiseplan/speiseplan_{date}_{mensa_dict[mensa_name]}_-de.html'
+url = f'https://www.studentenwerk-muenchen.de/mensa/speiseplan/speiseplan_{str(date_today)}_{mensa_dict[mensa_name]}_-de.html'
 
 # do HTML request and create BeautifulSoup object
 res = requests.get(url)
@@ -18,8 +26,8 @@ dish_descriptions = soup.find_all(class_='js-schedule-dish-description')
 artnames = soup.find_all(class_='stwm-artname')
 
 # print out the mensa menu
-print(f'Speiseplan {mensa_name} am {date}:\n')
 for i, dish_description in enumerate(dish_descriptions):
+print(f'Speiseplan {mensa_name} am {str(date_today)}:\n')
 
     # if artname is not there then just print an empty string instead
     if len(artnames[i]) > 0:
