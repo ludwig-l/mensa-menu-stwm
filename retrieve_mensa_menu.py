e594a090f3afcc6f13a2837baa5f93a955890a27
Ludwig Lohmer <ludwig-lohmer@web.de>
2022-07-13 22:02:56 +0200

    Add argparser to script
    features:
    date, mensa and language (translation not implemented yet)

--- retrieve_mensa_menu.py
+++ retrieve_mensa_menu.py
@@ -2,19 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import date
 import sys
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--lang', nargs='?', default='de', help='language to translate to (e.g \"en\")', type=str)
+parser.add_argument('-d', '--date', nargs='?', default='today', help='specify a specific date to retrieve the menu plan for (in format YYYY-MM-DD)', type=str)
+parser.add_argument('-m', '--mensa', nargs='?', default='Mensa Arcisstraße', help='choose the mensa to retrieve the menu plan for (e.g. \"Mensa Arcisstraße\"', type=str)
+args = parser.parse_args()
   
 # Returns the current local date
-date_today = date.today()
+if args.date == 'today':
+    date = date.today()
+else:
+    date = date.fromisoformat(args.date)
 
 # check if today is a weekday
-if date_today.weekday() >= 5:
-    print('Today is not a weekday. The mensa is not open today.')
+if date.weekday() >= 5:
+    if args.date == 'today':
+        print('Today is not a weekday. The mensa is not open today.')
+    else:
+        print('The specified day is not a weekday. The mensa is not open on that day.')
     sys.exit()
 
 # definitions
 mensa_dict = {'Mensa Arcisstraße': 421, 'Mensa Leopoldstraße': 411, 'Mensa Garching': 422}
-mensa_name = 'Mensa Arcisstraße'
-url = f'https://www.studentenwerk-muenchen.de/mensa/speiseplan/speiseplan_{str(date_today)}_{mensa_dict[mensa_name]}_-de.html'
+url = f'https://www.studentenwerk-muenchen.de/mensa/speiseplan/speiseplan_{str(date)}_{mensa_dict[args.mensa]}_-de.html'
 
 # do HTML request and create BeautifulSoup object
 res = requests.get(url)
@@ -26,7 +39,7 @@ dish_descriptions = soup.find_all(class_='js-schedule-dish-description')
 artnames = soup.find_all(class_='stwm-artname')
 
 # print out the mensa menu
-print(f'Speiseplan {mensa_name} am {str(date_today)}:\n')
+print(f'Speiseplan {args.mensa} am {str(date)}:\n')
 for i, (dish_description, artname) in enumerate(zip(dish_descriptions, artnames)):
 
     # if artname is not there then just print an empty string instead
